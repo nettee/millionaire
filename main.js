bfd6195feedfc89ff85b3fc0abb6df67abeffdc6
William <nettee.liu@gmail.com>
2019-08-15 23:23:11 +0800

    设置启动最大化

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+
+  mainWindow.maximize()
+  mainWindow.setResizable(false)
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
